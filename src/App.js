161cf0ac11d64725dc5c3aecf9368da6523a8237
subhash yadav <subhash41828@gmail.com>
2023-02-16 21:13:24 +0545

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [user, setUser] = useState([]);
+  const getUser = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    setUser(data);
+  };
+  const [querry, setQuerry] = useState("");
+  
+  const userQuerry = (e) => {
+    setQuerry(e.target.value)
+  }
+ 
+console.log(querry,"Querry")
+  useEffect(() => {
+    getUser();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input className="App-input" type="text" placeholder="Search anyThing" onChange={userQuerry} />
+      <ul>
+        {user.filter((users)=>users.name.toLowerCase().includes(querry)).map((user) => {
+          return <li key={user.id}>{user.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
